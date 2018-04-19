3906d2614c5c1b158f9a6e6fe222bb40a6ececef
RJ Nowling <rnowling@gmail.com>
2018-04-18 20:05:01 -0500

    Hardy-multiple is compiling

--- hardy-multiple/src/main.rs
+++ hardy-multiple/src/main.rs
@@ -2,9 +2,9 @@ extern crate rand;
 
 use rand::{Rng, thread_rng};
 use rand::distributions::{Distribution, Range};
-use std::collections::BTreeMap;
-use std::cmp::Ordering;
-use std::fmt;
+// use std::collections::BTreeMap;
+// use std::cmp::Ordering;
+// use std::fmt;
 use std::mem;
 
 // Simulation of Hardy model
@@ -14,21 +14,22 @@ use std::mem;
 // There is no mutation and the sex of the parents doesn't matter.
 // Mating is random.
 
-type Locus = u8; // each allele had a particular locus is represented as an integer
-type Gamete = Vec<Locus>;
-type Chromosome = Vec<Gamete>; // a haploid organism has a single gamete per chromosome, a diploid organism has two gametes per chromosome, and so forth
-type Individual = Vec<Chromosome>;
+pub type Locus = u8; // each allele had a particular locus is represented as an integer
+pub type Gamete = Vec<Locus>;
+pub type Chromosome = Vec<Gamete>; // a haploid organism has a single gamete per chromosome, a diploid organism has two gametes per chromosome, and so forth
+pub type Individual = Vec<Chromosome>;
 
-struct ChromosomeArchitecture {
+#[derive(Clone)]
+pub struct ChromosomeArchitecture {
     pub n_loci: usize,
-    pub n_alleles_per_locus: Vec<usize>
+    pub n_alleles_per_locus: Vec<u8>
 }
 
 impl ChromosomeArchitecture {
-    pub fn new(n_loci: usize, n_alleles_per_locus: &Vec<usize>) -> ChromosomeArchitecture {
+    pub fn new(n_loci: usize, n_alleles_per_locus: Vec<u8>) -> ChromosomeArchitecture {
         ChromosomeArchitecture {
             n_loci: n_loci,
-            n_alleles_per_locus: *n_alleles_per_locus
+            n_alleles_per_locus: n_alleles_per_locus
         }
     }
 }
@@ -36,7 +37,8 @@ impl ChromosomeArchitecture {
 // Describe the organism
 //
 //
-struct GeneticArchitecture {
+#[derive(Clone)]
+pub struct GeneticArchitecture {
     pub n_chromosomes: usize,
     pub n_gametes: usize,
     pub chromosome_architectures: Vec<ChromosomeArchitecture>
@@ -53,7 +55,7 @@ impl GeneticArchitecture {
         }
     }
 
-    pub fn add_chromosome(&mut self, n_loci: usize, n_alleles_per_locus: &Vec<usize>) -> () {
+    pub fn add_chromosome(&mut self, n_loci: usize, n_alleles_per_locus: Vec<u8>) -> () {
         let chromosome = ChromosomeArchitecture::new(n_loci, n_alleles_per_locus);
         
         self.n_chromosomes += 1;
@@ -107,22 +109,22 @@ struct Population {
 //     }
 // }
 
-pub trait IndividualSampler<R: Rng> {
-    fn sample(&mut self, genetic_architecture: &GeneticArchitecture) -> Individual;
+pub trait IndividualSampler {
+    fn sample<R: Rng>(&mut self, genetic_architecture: &GeneticArchitecture, rng: &mut R) -> Individual;
 }
 
 impl Population {
-    pub fn new<R: Rng, S: IndividualSampler<R>>(genetic_architecture: &GeneticArchitecture, n: usize, factory: &mut S) -> Population {
+    pub fn new<R: Rng>(genetic_architecture: &GeneticArchitecture, n: usize, sampler: &mut UniformSampler, rng: &mut R) -> Population {
         let mut parents: Vec<Individual> = Vec::new();
         let mut children: Vec<Individual> = Vec::new();
 
         for _ in 0..n {
-            parents.push(factory.sample(genetic_architecture));
-            children.push(factory.sample(genetic_architecture));
+            parents.push(sampler.sample(genetic_architecture, rng));
+            children.push(sampler.sample(genetic_architecture, rng));
         }
 
         Population {
-            genetic_architecture: *genetic_architecture,
+            genetic_architecture: genetic_architecture.clone(),
             n_individuals: n,
             parents: parents,
             children: children
@@ -170,7 +172,7 @@ impl Population {
                     let parent_chromosome = &parent[i];
 
                     // this is probably causing a copy...
-                    chromosome.push(parent_chromosome[gamete_range.sample(rng)]);
+                    chromosome.push(parent_chromosome[gamete_range.sample(rng)].clone());
                 }
             }
         }
@@ -178,7 +180,41 @@ impl Population {
 }
 
 struct UniformSampler {
-    
+
+}
+
+impl UniformSampler {
+    pub fn new() -> UniformSampler {
+        UniformSampler {
+
+        }
+    }
+}
+
+impl IndividualSampler for UniformSampler {
+    fn sample<R: Rng>(&mut self, gen_arch: &GeneticArchitecture, rng: &mut R) -> Individual {
+        let mut individual: Individual = Vec::new();
+        let n_gamete = gen_arch.n_gametes;
+
+        for chrom_arch in gen_arch.chromosome_architectures.iter() {
+            let mut chromosome: Chromosome = Vec::new();
+
+            for _ in 0..n_gamete {
+                let mut gamete: Gamete = Vec::new();
+
+                for alleles_per_locus in &chrom_arch.n_alleles_per_locus {
+                    let allele = rng.gen_range(0u8, *alleles_per_locus);
+                    gamete.push(allele);
+                }
+
+                chromosome.push(gamete);
+            }
+
+            individual.push(chromosome);
+        }
+
+        individual
+    }
 }
 
 fn main() {
@@ -192,13 +228,15 @@ fn main() {
 
     // 2 chromosomes (each chromosome has 2 gametes)
     // each chromosome has 2 loci of 2 alleles
-    genetic_architecture.add_chromosome(2usize, &vec![2usize, 2usize]);
-    genetic_architecture.add_chromosome(2usize, &vec![2usize, 2usize]);
+    genetic_architecture.add_chromosome(2usize, vec![2u8, 2u8]);
+    genetic_architecture.add_chromosome(2usize, vec![2u8, 2u8]);
 
+    let mut sampler = UniformSampler::new();
     let mut population = Population::new(&genetic_architecture,
                                          population_size,
+                                         &mut sampler,
                                          &mut rng);
-    for i in 0..generations {
+    for _i in 0..generations {
         // println!("Population: {}", i);
         // for (genotype, freq) in &population.genotype_frequencies() {
         //     println!("Genotype: {}, Frequency: {}", genotype, freq);
